4ff21d2ac927409282b6a1a6d8516387637cc320
AnnsuChoudhary <annsuchoudhary2004@gmail.com>
2026-08-06 06:24:42 +0000

    feat: Add Maurya Dynasty explorer and tests

--- frontend/history/dynasties/data.js
+++ frontend/history/dynasties/data.js
@@ -4,6 +4,8 @@ export const dynasties = [
     name: "Maurya Empire",
     period: "322–185 BCE",
     startYear: -322,
+    explorerPath: "../Maurya Dynasty/MauryaDynasty.html",
+    explorerLabel: "Open Maurya Explorer",
     endYear: -185,
     capital: "Pataliputra (modern Patna)",
     founders: "Chandragupta Maurya",
