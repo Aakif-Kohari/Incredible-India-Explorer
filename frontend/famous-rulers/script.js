d2775f09b0026acffcea6d17bf414efd9bfc9b90
prasiddhi-105 <bhumimishra91@gmail.com>
2026-08-18 10:03:27 +0530

    feat: Add Krishnadevaraya Greatest Ruler of the Vijayanagara Empire #2271

--- frontend/famous-rulers/script.js
+++ frontend/famous-rulers/script.js
@@ -25,6 +25,16 @@ const rulersData = [
     link: "../ashoka-the-great-explorer/index.html"
   },
   {
+    name: "Krishnadevaraya",
+    era: "Medieval",
+    dynasty: "Vijayanagara",
+    region: "South India",
+    reign: "1509 – 1529 CE",
+    image: "krishnadev.webp", 
+    description: "The greatest emperor of the Vijayanagara Empire. A military genius and a celebrated scholar, his reign saw exceptional architectural and literary achievements at Hampi.",
+    link: "krishnadevaraya.html" 
+  }, 
+  {
     name: "Samudragupta",
     era: "Ancient",
     dynasty: "Gupta",
